92b9c475e3cd0656887086eb86d379dd4bdcecdd
Ivan <jonnykold@mail.ru>
2023-05-25 23:18:22 +0200

    Add tag to determine if user liked comment

--- blogs/templatetags/blogs_helpers.py
+++ blogs/templatetags/blogs_helpers.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.simple_tag
 def user_has_liked_post(post, user):
     return post.has_liked(user)
+
+
+@register.simple_tag
+def user_has_liked_comment(comment, user):
+    return comment.has_liked(user)
