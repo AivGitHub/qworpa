6a261d758d706c8e15a339c56c6304288c75de6d
Ivan <jonnykold@mail.ru>
2023-05-25 23:06:58 +0200

    Add api to toggle comment like

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from api.views import delete_post, toggle_post_like
+from api.views import delete_post, toggle_post_comment_like, toggle_post_like
 
 urlpatterns = [
     path('v1/posts/<str:url_hex>/likes/toggle/', toggle_post_like, name='toggle-post-like'),
     path('v1/posts/<str:url_hex>/delete/', delete_post, name='delete-post'),
+    path(
+        'v1/posts/<str:url_hex>/comments/<int:comment_id>/likes/toggle/',
+        toggle_post_comment_like,
+        name='toggle-post-comment-like'
+    ),
 ]
--- api/views.py
+++ api/views.py
@@ -5,7 +5,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from accounts.models import User
-from blogs.models import Post, PostLike
+from blogs.models import Post, PostComment, PostCommentLike, PostLike
 
 
 def _get_post_from_payload(payload: dict, user: User):
@@ -52,4 +52,37 @@ def process_delete_post(user: User, payload: dict):
 @permission_classes([IsAuthenticated])
 def delete_post(request, *args, **kwargs):
     process_delete_post(request.user, kwargs)
+    return Response(status=status.HTTP_200_OK)
+
+
+def _get_comment_from_payload(payload: dict, user: User):
+    try:
+        comment_id = payload['comment_id']
+    except KeyError:
+        raise PermissionDenied()
+    # Check that user makes a request from a page with post.
+    # Can be removed for now to optimise the request.
+    post = _get_post_from_payload(payload, user)
+    try:
+        post_comment: PostComment = post.comments.get(id=comment_id)
+    except PostComment.DoesNotExist:
+        raise PermissionDenied()
+
+    return post_comment
+
+
+def process_toggle_post_comment_like(user: User, payload: dict):
+    post_comment: PostComment = _get_comment_from_payload(payload, user)
+    try:
+        post_comment_like = PostCommentLike.objects.get(user=user, comment=post_comment)
+    except PostCommentLike.DoesNotExist:
+        PostCommentLike.objects.create(user=user, comment=post_comment)
+    else:
+        post_comment_like.delete()
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def toggle_post_comment_like(request, *args, **kwargs):
+    process_toggle_post_comment_like(request.user, kwargs)
     return Response(status=status.HTTP_200_OK)
