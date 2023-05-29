faa73a95a55415851ffba1643697937a4b03af58
Ivan <jonnykold@mail.ru>
2023-05-29 22:10:48 +0200

    Increase page size limits

--- api/views.py
+++ api/views.py
@@ -180,9 +180,9 @@ def add_post_comment(request, *args, **kwargs):
 
 
 class Pagination(PageNumberPagination):
-    page_size = 10
+    page_size = 50
     page_size_query_param = 'page_size'
-    max_page_size = 50
+    max_page_size = 100
     page_query_param = 'p'
 
 
