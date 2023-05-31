bfb6712009578bf72094e6eef8e802b718dc01e8
Ivan <jonnykold@mail.ru>
2023-05-31 21:03:02 +0200

    Adjust port to heroku

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+if [ -z "${PORT}" ]
+then
+  PORT=8080
+fi
+
 python manage.py collectstatic --noinput
 python manage.py migrate
 
-uwsgi --http :8080 --ini configurations/server.ini --static-map /static=static --static-map /favicon.ico=favicon.ico
+uwsgi --http :$PORT --ini configurations/server.ini --static-map /static=static --static-map /favicon.ico=favicon.ico
