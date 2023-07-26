f144aa33668dda0e37fdc8f20695d48f11e8fa0c
Ivan <jonnykold@mail.ru>
2023-07-26 22:05:38 +0200

    Add helper to determine if user subscribed

--- blogs/templatetags/blogs_helpers.py
+++ blogs/templatetags/blogs_helpers.py
@@ -11,3 +11,10 @@ def user_has_liked_post(post, user):
 @register.simple_tag
 def user_has_liked_comment(comment, user):
     return comment.has_liked(user)
+
+
+@register.simple_tag
+def user_has_subscribed(user, author):
+    if user.is_anonymous:
+        return False
+    return author.subscribers.contains(user)
