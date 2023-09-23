71791054aa2de34c7f85c1d2881984708ca5e1b4
Ivan <jonnykold@mail.ru>
2023-09-23 22:48:45 +0200

    Remove ads

--- accounts/static/accounts/js/popunder_ads.js
+++
@@ -1,8 +0,0 @@
-(function(eyt){
-var d = document,
-    s = d.createElement('script'),
-    l = d.scripts[d.scripts.length - 1];
-s.settings = eyt || {};
-s.src = "\/\/crookedchange.com\/a\/WO5.w-YMW\/d_lBQp2f9ikuZjTd9G6Wbe2G5jlvSMWnQ\/9AN\/D\/gzy\/MMDCMt0\/NfSW0i0sOMD\/IDwwMOzbQn2O";
-l.parentNode.insertBefore(s, l);
-})({});
